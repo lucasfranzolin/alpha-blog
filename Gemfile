da4c82c33c4fe40d11ee1501a2ca202e866bea0e
Lucas Franzolin <lucasfranzolin@gmail.com>
2018-07-17 11:59:42 -0300

    add categories model and tests

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'bcrypt', '~> 3.1.7'
 gem 'will_paginate', '~> 3.1.5'
 gem 'bootstrap-will_paginate', '~> 1.0.0'
+gem 'rails-controller-testing'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
---
+++ test/models/category_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  def setup
+    @category = Category.new(name: "sports")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid? # tem que dar false
+  end
+
+  test "name sould be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26 # aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
+end
